394a87b5ecb747224a467cf15c99e4db750f1135
Manuel <info@tagirijus.de>
2023-03-28 14:25:12 +0200

    Added hover display in board columns

--- Template/board/column_hours.php
+++ Template/board/column_hours.php
@@ -1,5 +1,16 @@
-<?php $times = $tagiTimes($column); ?>
+<?php
 
-<div class="thv-column-wrapper">
+$times = $tagiTimes($column);
+$hover_text = t('Estimated') . ': ' . $this->hoursViewHelper->floatToHHMM($times['estimated']) . 'h';
+$hover_text .= "\n";
+$hover_text .= t('Spent') . ': ' . $this->hoursViewHelper->floatToHHMM($times['spent']) . 'h';
+$hover_text .= "\n";
+$hover_text .= t('Remaining') . ': ' . $this->hoursViewHelper->floatToHHMM($times['remaining']) . 'h';
+
+?>
+
+
+<div class="thv-column-wrapper" title="<?= $hover_text ?>">
+    <span class="ui-helper-hidden-accessible"><?= $hover_text ?></span>
     <span class="thv-spent-color"><?= round($times['spent'], 2); ?></span>/<span class="thv-estimated-color"><?= round($times['estimated'], 2); ?></span>h
 </div>
