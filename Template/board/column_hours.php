16dd9a82ef5d73447d519b6e276bf4b2499a1d8c
Manuel <info@tagirijus.de>
2023-06-02 09:49:50 +0200

    Column reamining hours now bigger

--- Template/board/column_hours.php
+++ Template/board/column_hours.php
@@ -15,5 +15,5 @@ $hover_text .= t('Overtime') . ': ' . $this->hoursViewHelper->floatToHHMM($times
 <div class="thv-column-wrapper thv-font-small" title="<?= $hover_text ?>">
     <span class="ui-helper-hidden-accessible"><?= $hover_text ?></span>
     <span class="thv-spent-color"><?= $this->hoursViewHelper->floatToHHMM(round($times['spent'], 2)); ?></span>/<span class="thv-estimated-color"><?= $this->hoursViewHelper->floatToHHMM(round($times['estimated'], 2)); ?></span><br>
-    <span class="thv-remaining-color"><?= $this->hoursViewHelper->floatToHHMM(round($times['remaining'], 2)); ?></span>
+    <span class="thv-remaining-color thv-font-big"><?= $this->hoursViewHelper->floatToHHMM(round($times['remaining'], 2)); ?></span>
 </div>
