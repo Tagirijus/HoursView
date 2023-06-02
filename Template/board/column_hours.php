7fbdf9ec0b21f58f5c95a10c48c2ee70d4c9f118
Manuel <info@tagirijus.de>
2023-06-02 08:24:16 +0200

    Added remaining time in column header

--- Template/board/column_hours.php
+++ Template/board/column_hours.php
@@ -6,11 +6,14 @@ $hover_text .= "\n";
 $hover_text .= t('Spent') . ': ' . $this->hoursViewHelper->floatToHHMM($times['spent']) . 'h';
 $hover_text .= "\n";
 $hover_text .= t('Remaining') . ': ' . $this->hoursViewHelper->floatToHHMM($times['remaining']) . 'h';
+$hover_text .= "\n";
+$hover_text .= t('Overtime') . ': ' . $this->hoursViewHelper->floatToHHMM($times['overtime']) . 'h';
 
 ?>
 
 
 <div class="thv-column-wrapper thv-font-small" title="<?= $hover_text ?>">
     <span class="ui-helper-hidden-accessible"><?= $hover_text ?></span>
-    <span class="thv-spent-color"><?= $this->hoursViewHelper->floatToHHMM(round($times['spent'], 2)); ?></span>/<span class="thv-estimated-color"><?= $this->hoursViewHelper->floatToHHMM(round($times['estimated'], 2)); ?></span>h
+    <span class="thv-spent-color"><?= $this->hoursViewHelper->floatToHHMM(round($times['spent'], 2)); ?></span>/<span class="thv-estimated-color"><?= $this->hoursViewHelper->floatToHHMM(round($times['estimated'], 2)); ?></span><br>
+    <span class="thv-remaining-color"><?= $this->hoursViewHelper->floatToHHMM(round($times['remaining'], 2)); ?></span>
 </div>
